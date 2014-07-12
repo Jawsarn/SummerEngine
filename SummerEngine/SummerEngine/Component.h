8b43d67be5336a006baeafc02d099aa2776337f7
Jawsarn <christian_markowicz@hotmail.com>
2014-07-12 18:13:46 +0200

    Fixed Component.h

--- SummerEngine/SummerEngine/Component.h
+++ SummerEngine/SummerEngine/Component.h
@@ -1,8 +1,21 @@
 #pragma once
+#include <string>
+
 class Component
 {
 public:
 	Component();
 	~Component();
+
+	virtual void Awake() = 0;
+	virtual void Start() = 0;
+	virtual void Update() = 0;
+	virtual void Destroy() = 0;
+
+
+private:
+	bool m_IsEnabled;
+	int m_EntityID;
+	std::string m_Name; //not sure if needed
 };
 
