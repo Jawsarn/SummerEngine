80519a8ecd598d10243332579816176efd456a40
MostafaHassan <mostafahassan93@hotmail.com>
2014-07-13 15:36:24 +0200

    MeshComponent

--- SummerEngine/SummerEngine/MeshComponent.cpp
+++ SummerEngine/SummerEngine/MeshComponent.cpp
@@ -9,3 +9,18 @@ MeshComponent::MeshComponent()
 MeshComponent::~MeshComponent()
 {
 }
+
+Mesh* MeshComponent::GetMesh()
+{
+	return m_Mesh;
+}
+
+bool MeshComponent::IsCastShadows()
+{
+	return m_CastShadows;
+}
+
+bool MeshComponent::IsReceiveShadows()
+{
+	return m_ReceiveShadows;
+}
