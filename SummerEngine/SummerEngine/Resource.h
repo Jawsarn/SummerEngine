bf325fe152c028666d40a06fdb49b02aabaa0892
Jawsarn <christian_markowicz@hotmail.com>
2014-07-14 00:42:22 +0200

    Resource changes

--- SummerEngine/SummerEngine/Resource.h
+++ SummerEngine/SummerEngine/Resource.h
@@ -1,8 +1,16 @@
 #pragma once
+#include <string>
+
 class Resource
 {
 public:
 	Resource();
 	~Resource();
+
+	virtual bool Load();
+	virtual void Save();
+private:
+	std::string m_ResourceName;
+	
 };
 
