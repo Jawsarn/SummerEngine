61f7bb1082c92053c4c4a3682e1df6e879ba4c1d
MostafaHassan <mostafahassan93@hotmail.com>
2014-07-13 19:47:09 +0200

    Working on loading obj files

--- SummerEngine/SummerEngine/LoadObj.cpp
+++ SummerEngine/SummerEngine/LoadObj.cpp
@@ -1,6 +1,5 @@
 #include "LoadObj.h"
 
-
 LoadObj::LoadObj()
 {
 
@@ -13,8 +12,102 @@ LoadObj::~LoadObj()
 
 bool LoadObj::Load(char* p_fileName)
 {
-	//load crap here :P!
+	m_CurrentGroup = 0;
+	std::ifstream t_file;
+	t_file.open(p_fileName);
+
+	if (!t_file.is_open())
+	{
+		//MessageBox(nullptr, L"Mesh could not be loaded", L"Error", MB_ICONERROR | MB_OK);
+		return false;
+	}
+
+	std::string stream_line;
+
+	while (std::getline(t_file, stream_line))
+	{
+		std::stringstream str_stream(stream_line);
+		std::string type_str;
+		str_stream >> type_str;
+
+		if (type_str == "#" || type_str == "s")
+		{
+			//comment = ignore
+			//ignoring smooth for now
+		}
+
+		else if (type_str == "v")
+		{
+			LoadPosition(str_stream);
+		}
+
+		else if (type_str == "vn")
+		{
+			LoadNormal(str_stream);
+		}
+
+		else if (type_str == "vt")
+		{
+			LoadTexCoord(str_stream);
+		}
+
+		else if (type_str == "g")
+		{
+			LoadGroup(str_stream);
+		}
+
+		else if (type_str == "f")
+		{
+			LoadFace(str_stream);
+		}
+
+		else if (type_str == "usemtl")
+		{
+
+		}
+	}
+
+	t_file.close();
 	return true;
+}
+
+void LoadObj::LoadGroup(std::stringstream& f)
+{
+	if (m_groups.size() > 0)
+	{
+		m_CurrentGroup += 1;
+	}
+
+	ObjGroups t_group;
+	f >> t_group.name;
+	m_groups.push_back(t_group);
+
+}
+
+void LoadObj::LoadFace(std::stringstream& f)
+{
+	//f 2/1/1 3/2/1 4/3/1
+}
+
+void LoadObj::LoadPosition(std::stringstream& f)
+{
+	XMFLOAT3 t_pos;
+	f >> t_pos.x >> t_pos.y >> t_pos.z;
+	int a = 0;
+	//m_groups[m_CurrentGroup].triangles.push_back()
+
+}
+
+void LoadObj::LoadNormal(std::stringstream& f)
+{
+	XMFLOAT3 t_norm;
+	f >> t_norm.x >> t_norm.y >> t_norm.z;
+}
+
+void LoadObj::LoadTexCoord(std::stringstream& f)
+{
+	XMFLOAT2 t_texCoord;
+	f >> t_texCoord.x >> t_texCoord.y;
 }
 
 XMFLOAT3& LoadObj::GetPosition(int p_index)
--- SummerEngine/SummerEngine/LoadObj.h
+++ SummerEngine/SummerEngine/LoadObj.h
@@ -2,16 +2,21 @@
 #include <string>
 #include <vector>
 #include "Math.h"
+#include <fstream>
+#include <sstream>
 
-struct ObjGroup
+struct ObjGroups
 {
-	struct Triangles
+	struct Triangle
 	{
-		int vertex[3][3];
+		//int vertex[3][3];
+		XMFLOAT3 position;
+		XMFLOAT3 normal;
+		XMFLOAT2 texCoord;
 	};
 	std::string name;
 	std::string material;
-	std::vector<Triangles> triangles;
+	std::vector<Triangle> triangles;
 };
 
 class LoadObj
@@ -20,15 +25,22 @@ public:
 	LoadObj();
 	~LoadObj();
 
-	ObjGroup* m_CurrentGroup;
+	std::vector<ObjGroups> m_groups;
 	bool Load(char* p_fileName);
 	XMFLOAT3& GetPosition(int p_index);
 	XMFLOAT3& GetNormal(int p_index);
 	XMFLOAT3& GetTexCoord(int p_index);
 
 private:
+	int m_CurrentGroup;
 	std::vector<XMFLOAT3> m_Position;
 	std::vector<XMFLOAT3> m_Normal;
 	std::vector<XMFLOAT3> m_TexCoord;
+
+	void LoadGroup(std::stringstream& f);
+	void LoadFace(std::stringstream& f);
+	void LoadPosition(std::stringstream& f);
+	void LoadNormal(std::stringstream& f);
+	void LoadTexCoord(std::stringstream& f);
 };
 
