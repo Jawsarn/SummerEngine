56624240154e34a166c80d9bc4467f46a351f805
MostafaHassan <mostafahassan93@hotmail.com>
2014-07-14 21:10:10 +0200

    Started with creating the obj

--- SummerEngine/SummerEngine/Mesh.cpp
+++ SummerEngine/SummerEngine/Mesh.cpp
@@ -9,3 +9,23 @@ Mesh::Mesh()
 Mesh::~Mesh()
 {
 }
+
+void Mesh::Release()
+{
+
+}
+
+
+HRESULT Mesh::CreateMesh(ID3D11Device* p_device, char* p_fileName)
+{
+	mesh->Load(p_fileName);
+
+	//Amount of meshes (groups-objects) in file.
+	for (int i = 0; i < mesh->GetGroupCount(); i++)
+	{
+
+	}
+
+	return S_OK;
+}
+
--- SummerEngine/SummerEngine/Mesh.h
+++ SummerEngine/SummerEngine/Mesh.h
@@ -1,13 +1,30 @@
 #pragma once
-#include "Math.h"
 #include "LoadObj.h"
+#include "Math.h"
+
 class Mesh
 {
 public:
 	Mesh();
 	~Mesh();
 
+
 private:
+	LoadObj* mesh;
+	void Release();
+	HRESULT CreateMesh(ID3D11Device* p_device, char* p_fileName);
 
+	struct MeshVertex
+	{
+		XMFLOAT3 position;
+		XMFLOAT3 normal;
+		XMFLOAT2 texCoord;
+
+		MeshVertex()
+		{
+			position = XMFLOAT3(0, 0, 0);
+			normal = XMFLOAT3(0, 0, 0);
+			texCoord = XMFLOAT2(0, 0);
+		}
+	};
 };
-
