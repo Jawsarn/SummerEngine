c3e57ea7b449511dcb7647879c5afe68293d3b8c
Jawsarn <christian_markowicz@hotmail.com>
2014-07-16 14:50:41 +0200

    Some small change

--- SummerEngine/SummerEngine/ResourceManager.h
+++ SummerEngine/SummerEngine/ResourceManager.h
@@ -9,9 +9,9 @@ public:
 	ResourceManager();
 	~ResourceManager();
 
-	Resource* CreateTexture();
-	Resource* CreateAnimation();
-	Resource* CreateMesh();
+	Resource* CreateTexture(std::string p_ResourceName);
+	Resource* CreateAnimation(std::string p_ResourceName);
+	Resource* CreateMesh(std::string p_ResourceName);
 
 private:
 	std::map<std::string, Resource*> m_Resources;
