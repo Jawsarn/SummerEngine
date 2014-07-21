2a2de1aae5e1eb7035944d4036b00da2376aa0c5
MostafaHassan <mostafahassan93@hotmail.com>
2014-07-21 18:41:14 +0200

    Almost done with the new structure of importing models through the editor (seperation of each mesh in obj file)

--- SummerEngine/SummerEngine/Import.cpp
+++ SummerEngine/SummerEngine/Import.cpp
@@ -30,112 +30,190 @@ void Import::LoadFromObj(ID3D11Device* p_Device, std::string p_FileName)
 	m_LoadObj->Load(p_FileName);
 	//gameObject = GameObject(p_Device);
 
-	int t_ObjCount = m_LoadObj->GetObjCount();
+	int t_ObjMeshCount = m_LoadObj->GetObjCount();
 	int t_GroupCount = m_LoadObj->GetGroupCount();
 
-	std::vector<ObjGroups> t_CurGroup;
-
-	//Get all groups from all meshes in scene
-	std::vector<std::vector<ObjGroups>> t_AllGroups;
-	for (int i = 0; i < t_ObjCount; i++)
+	//This is a vector of meshes which contains the material groups(triangles)
+	std::vector<std::vector<ObjGroups>> t_Meshes;
+	for (int i = 0; i < t_ObjMeshCount; i++)
 	{
-		t_AllGroups.push_back(m_LoadObj->GetAllGroupsFromAMesh(i));
+		t_Meshes.push_back(m_LoadObj->GetAllGroupsFromAMesh(i));
 	}
 
-	//Set them to a vector
-	int t_AllGroupsSize = t_AllGroups.size();
-	for (int i = 0; i < t_AllGroupsSize; i++)
+	//Iterate through all the meshes
+	for (int i = 0; i < t_ObjMeshCount; i++)
 	{
-		for (int x = 0; x < t_AllGroups[i].size(); x++)
+		int t_GroupCount = t_Meshes[i].size();
+		std::vector<ObjGroups> t_CurGroup(t_GroupCount);
+
+		//MaterialData* t_GroupMaterial;
+
+		for (int x = 0; x < t_GroupCount; x++)
 		{
-			t_CurGroup.push_back(t_AllGroups[i][x]);
+			//Assaign the current group to the meshes index group
+			t_CurGroup[x] = t_Meshes[i][x];
+
+			//ID3D11Buffer* buffer = nullptr;
+
+			Mesh::MeshVertex t_V;
+			ObjGroups::Triangle* t_Triangle = nullptr;
+
+			int count = 0;
+			int t_TriangleSize = t_CurGroup[x].triangles.size();
+
+			std::vector<Mesh::MeshVertex> t_Vertices(t_TriangleSize * 3);
+
+			//std::string t_MaterialName = t_CurGroup[x].material;
+			//t_GroupMaterial = m_LoadObj->GetMaterial(t_MaterialName);
+
+			for (int y = 0; y < t_TriangleSize; y++)
+			{
+				//triangle has all the indexes for v/vn/vt
+				t_Triangle = &t_CurGroup[x].triangles[y];
+				int t_ObjIndex = t_CurGroup[x].m_ObjId;
+
+				//Creating the triangle
+				for (int j = 2; j >= 0; j--)
+				{
+					//iterate throug all triangles and extract the position from the vectors
+					//index from triangle
+
+					//int test_PositionIndex = t_Triangle->index[j][0];
+					//int test_TexIndex = t_Triangle->index[j][1];
+					//int test_NormIndex = t_Triangle->index[j][2];
+
+					if (m_LoadObj->GetPositions(t_ObjIndex).size() == 0)
+					{
+						std::string t_ErrorMessage = "Could not find positions on an OBJ:" + p_FileName;
+						std::wstring t_MeshName(t_ErrorMessage.begin(), t_ErrorMessage.end());
+						MessageBox(nullptr, t_MeshName.c_str(), L"Error-OBJ", MB_ICONERROR | MB_OK);
+					}
+					else
+					{
+						//Get Positions
+						t_V.position = m_LoadObj->GetPositions(t_ObjIndex)[t_Triangle->index[j][0]];
+					}
+
+					if (m_LoadObj->GetTexCoords(t_ObjIndex).size() == 0)
+					{
+						std::string t_ErrorMessage = "Could not find UV-Coords on an OBJ:" + p_FileName;
+						std::wstring t_MeshName(t_ErrorMessage.begin(), t_ErrorMessage.end());
+						MessageBox(nullptr, t_MeshName.c_str(), L"Error-OBJ", MB_ICONERROR | MB_OK);
+					}
+
+					else
+					{
+						//Get Texture Coordinates
+						t_V.texCoord = m_LoadObj->GetTexCoords(t_ObjIndex)[t_Triangle->index[j][1]];
+					}
+
+					if (m_LoadObj->GetNormals(t_ObjIndex).size() == 0)
+					{
+						std::string t_ErrorMessage = "Could not find Normals on an OBJ:" + p_FileName;
+						std::wstring t_MeshName(t_ErrorMessage.begin(), t_ErrorMessage.end());
+						MessageBox(nullptr, t_MeshName.c_str(), L"Error-OBJ", MB_ICONERROR | MB_OK);
+					}
+					else
+					{
+						//Get Normals
+						t_V.normal = m_LoadObj->GetNormals(t_ObjIndex)[t_Triangle->index[j][2]];
+					}
+					t_Vertices[count] = t_V;
+					//t_Vertices.push_back(t_V);
+
+					count += 1;
+				}
+			}
+
+			m_NumberOfVerticesInTotal += count;
+			m_Groups.push_back(t_Vertices);
+			t_Vertices.clear();
 		}
 	}
-	//clear old one
-	t_AllGroups.clear();
 
-	int t_CurGroupCount = t_CurGroup.size();
+
+	/*
 
 	//MaterialData* t_GroupMaterial;
 
 	for (int i = 0; i < t_CurGroupCount; i++)
 	{
-		//ID3D11Buffer* buffer = nullptr;
+	//ID3D11Buffer* buffer = nullptr;
 
-		Mesh::MeshVertex t_V;
-		ObjGroups::Triangle* t_Triangle = nullptr;
+	Mesh::MeshVertex t_V;
+	ObjGroups::Triangle* t_Triangle = nullptr;
 
-		int count = 0;
-		int t_TriangleSize = t_CurGroup[i].triangles.size();
+	int count = 0;
+	int t_TriangleSize = t_CurGroup[i].triangles.size();
 
-		std::vector<Mesh::MeshVertex> t_Vertices(t_TriangleSize * 3);
+	std::vector<Mesh::MeshVertex> t_Vertices(t_TriangleSize * 3);
 
-		//std::string t_MaterialName = t_CurGroup[i].material;
-		//t_GroupMaterial = m_Mesh->GetMaterial(t_MaterialName);
+	//std::string t_MaterialName = t_CurGroup[i].material;
+	//t_GroupMaterial = m_Mesh->GetMaterial(t_MaterialName);
 
-		for (int x = 0; x < t_TriangleSize; x++)
-		{
-			//triangle has all the indexes for v/vn/vt
-			t_Triangle = &t_CurGroup[i].triangles[x];
-			int t_ObjIndex = t_CurGroup[i].m_ObjId;
+	for (int x = 0; x < t_TriangleSize; x++)
+	{
+	//triangle has all the indexes for v/vn/vt
+	t_Triangle = &t_CurGroup[i].triangles[x];
+	int t_ObjIndex = t_CurGroup[i].m_ObjId;
 
-			//Creating the triangle
-			for (int j = 2; j >= 0; j--)
-			{
-				//iterate throug all triangles and extract the position from the vectors
-				//index from triangle
+	//Creating the triangle
+	for (int j = 2; j >= 0; j--)
+	{
+	//iterate throug all triangles and extract the position from the vectors
+	//index from triangle
 
-				//int test_PositionIndex = t_Triangle->index[j][0];
-				//int test_TexIndex = t_Triangle->index[j][1];
-				//int test_NormIndex = t_Triangle->index[j][2];
+	//int test_PositionIndex = t_Triangle->index[j][0];
+	//int test_TexIndex = t_Triangle->index[j][1];
+	//int test_NormIndex = t_Triangle->index[j][2];
 
-				if (m_LoadObj->GetPositions(t_ObjIndex).size() == 0)
-				{
-					std::string t_ErrorMessage = "Could not find positions on an OBJ:" + p_FileName;
-					std::wstring t_MeshName(t_ErrorMessage.begin(), t_ErrorMessage.end());
-					MessageBox(nullptr, t_MeshName.c_str(), L"Error-OBJ", MB_ICONERROR | MB_OK);
-				}
-				else
-				{
-					//Get Positions
-					t_V.position = m_LoadObj->GetPositions(t_ObjIndex)[t_Triangle->index[j][0]];
-				}
-
-				if (m_LoadObj->GetTexCoords(t_ObjIndex).size() == 0)
-				{
-					std::string t_ErrorMessage = "Could not find UV-Coords on an OBJ:" + p_FileName;
-					std::wstring t_MeshName(t_ErrorMessage.begin(), t_ErrorMessage.end());
-					MessageBox(nullptr, t_MeshName.c_str(), L"Error-OBJ", MB_ICONERROR | MB_OK);
-				}
-
-				else
-				{
-					//Get Texture Coordinates
-					t_V.texCoord = m_LoadObj->GetTexCoords(t_ObjIndex)[t_Triangle->index[j][1]];
-				}
-
-				if (m_LoadObj->GetNormals(t_ObjIndex).size() == 0)
-				{
-					std::string t_ErrorMessage = "Could not find Normals on an OBJ:" + p_FileName;
-					std::wstring t_MeshName(t_ErrorMessage.begin(), t_ErrorMessage.end());
-					MessageBox(nullptr, t_MeshName.c_str(), L"Error-OBJ", MB_ICONERROR | MB_OK);
-				}
-				else
-				{
-					//Get Normals
-					t_V.normal = m_LoadObj->GetNormals(t_ObjIndex)[t_Triangle->index[j][2]];
-				}
-				t_Vertices[count] = t_V;
-				//t_Vertices.push_back(t_V);
-
-				count += 1;
-			}
-		}
-		m_NumberOfVerticesInTotal += count;
-		m_Groups.push_back(t_Vertices);
-		t_Vertices.clear();
+	if (m_LoadObj->GetPositions(t_ObjIndex).size() == 0)
+	{
+	std::string t_ErrorMessage = "Could not find positions on an OBJ:" + p_FileName;
+	std::wstring t_MeshName(t_ErrorMessage.begin(), t_ErrorMessage.end());
+	MessageBox(nullptr, t_MeshName.c_str(), L"Error-OBJ", MB_ICONERROR | MB_OK);
+	}
+	else
+	{
+	//Get Positions
+	t_V.position = m_LoadObj->GetPositions(t_ObjIndex)[t_Triangle->index[j][0]];
 	}
 
+	if (m_LoadObj->GetTexCoords(t_ObjIndex).size() == 0)
+	{
+	std::string t_ErrorMessage = "Could not find UV-Coords on an OBJ:" + p_FileName;
+	std::wstring t_MeshName(t_ErrorMessage.begin(), t_ErrorMessage.end());
+	MessageBox(nullptr, t_MeshName.c_str(), L"Error-OBJ", MB_ICONERROR | MB_OK);
+	}
+
+	else
+	{
+	//Get Texture Coordinates
+	t_V.texCoord = m_LoadObj->GetTexCoords(t_ObjIndex)[t_Triangle->index[j][1]];
+	}
+
+	if (m_LoadObj->GetNormals(t_ObjIndex).size() == 0)
+	{
+	std::string t_ErrorMessage = "Could not find Normals on an OBJ:" + p_FileName;
+	std::wstring t_MeshName(t_ErrorMessage.begin(), t_ErrorMessage.end());
+	MessageBox(nullptr, t_MeshName.c_str(), L"Error-OBJ", MB_ICONERROR | MB_OK);
+	}
+	else
+	{
+	//Get Normals
+	t_V.normal = m_LoadObj->GetNormals(t_ObjIndex)[t_Triangle->index[j][2]];
+	}
+	t_Vertices[count] = t_V;
+	//t_Vertices.push_back(t_V);
+
+	count += 1;
+	}
+	}
+	m_NumberOfVerticesInTotal += count;
+	m_Groups.push_back(t_Vertices);
+	t_Vertices.clear();
+	}
+	*/
 	//CreateMeshBuffers(p_Device);
 }
 
