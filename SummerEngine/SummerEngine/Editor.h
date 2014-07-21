ecf4e777177f27eff340eb18b29bcc597240a3cd
MostafaHassan <mostafahassan93@hotmail.com>
2014-07-21 17:19:42 +0200

    Working on importer (obj) for editor

--- SummerEngine/SummerEngine/Editor.h
+++ SummerEngine/SummerEngine/Editor.h
@@ -6,5 +6,13 @@ class Editor
 public:
 	Editor();
 	~Editor();
+	void Release();
+	void InitEditor(ID3D11Device* p_Device);
+	void Update();
+	HRESULT Render(ID3D11DeviceContext* p_DeviceContext);
+
+private:
+	//Shader shader;//test
+	Import* m_import;
 };
 
