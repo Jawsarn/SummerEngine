fdc7ec6c6fc9378e54a35635a66e038696d4ffaa
MostafaHassan <mostafahassan93@hotmail.com>
2014-07-25 20:01:39 +0200

    Fixed Loading indecies binary

--- SummerEngine/SummerEngine/MeshResourceLoadSave.cpp
+++ SummerEngine/SummerEngine/MeshResourceLoadSave.cpp
@@ -9,6 +9,7 @@ Resource* MeshResourceLoadSave::Load(std::string p_FileName)
 	StreamFile t_File = StreamFile();
 	t_File.OpenFileRead(p_FileName);
 
+	//vertex
 	Mesh::VertexGroups t_Groups;
 
 	int t_NumOfGroups = ReadInt(t_File);
@@ -25,6 +26,22 @@ Resource* MeshResourceLoadSave::Load(std::string p_FileName)
 		t_Groups.push_back(t_VertexGroup);
 	}
 
+	//index
+	Mesh::IndicesGroups t_IndexGroups;
+
+	int t_NumOfIndecies = ReadInt(t_File);
+
+	for (int i = 0; i < t_NumOfIndecies; i++)
+	{
+		int t_NumOfIndecies = ReadInt(t_File);
+		
+		std::vector<int> t_IndexGroup;
+
+		t_IndexGroup.resize(t_NumOfIndecies);
+		t_File.Read(sizeof(int)*t_NumOfIndecies, &t_IndexGroup[0]);
+		t_IndexGroups.push_back(t_IndexGroup);
+	}
+
 	o_Mesh->SetVertexData(t_Groups);
 
 	return o_Mesh; //add code to create a mesh from file here
@@ -36,6 +53,7 @@ void MeshResourceLoadSave::Save(Resource* p_File)
 	
 	Mesh* t_Mesh = (Mesh*)p_File;
 	
+	//vertex
 	Mesh::VertexGroups* t_VertexGroups = t_Mesh->GetVertexData();
 
 	int t_NumOfGroups = t_VertexGroups->size();
@@ -49,6 +67,7 @@ void MeshResourceLoadSave::Save(Resource* p_File)
 		t_File.Write(sizeof(Mesh::MeshVertex)*t_NumOfVertices, &t_VertexGroups[i][0]);
 	}
 
+	//index
 	Mesh::IndicesGroups* t_IndexGroups = t_Mesh->GetIndexData();
 
 	int t_NumOfIndecies = t_IndexGroups->size();
